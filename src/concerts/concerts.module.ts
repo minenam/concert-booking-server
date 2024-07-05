049b26fece85a461f9377043f1439c0e26c129db
minenam <nhm6947@gmail.com>
2024-07-06 08:12:25 +0900

    feat: initialize auth, payment modules

--- src/concerts/concerts.module.ts
+++ src/concerts/concerts.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConcertsController } from './concerts.controller';
+import { ConcertsService } from './concerts.service';
 
-@Module({})
+@Module({
+  controllers: [ConcertsController],
+  providers: [ConcertsService],
+})
 export class ConcertsModule {}
