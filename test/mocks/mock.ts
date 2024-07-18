1245f693ce603183e24b0653fd1a2cdad655d59f
minenam <nhm6947@gmail.com>
2024-07-19 08:53:42 +0900

    test: Add auth service unit test

---
+++ test/mocks/mock.ts
@@ -0,0 +1,15 @@
+import { QueueStatus } from '@infrastructure/typeorm/entities/queue.entity';
+
+export const mockUser = {
+  id: 'some-user-id',
+  balance: 1000,
+  queue: [],
+};
+
+export const mockQueue = {
+  id: 1,
+  user: mockUser,
+  position: 1,
+  status: QueueStatus.WAITING,
+  createdAt: new Date(),
+};
