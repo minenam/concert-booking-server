c0efe961feaa4bb71ec95cc66b160915587e43e4
minenam <nhm6947@gmail.com>
2024-07-19 06:49:07 +0900

    chore: Add class validator and module mapper

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   const config = new DocumentBuilder()
     .setTitle('Concert Booking API')
     .setVersion('1.0')
@@ -11,7 +12,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
